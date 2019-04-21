a7da6c27cb8c24d40ae8a9bad1719e1b7fa18fb8
Pmastra <pabloo.agustin@gmail.com>
2019-04-21 18:18:15 -0300

    Test 2 lanzamientos normales

--- Bowling.java
+++ Bowling.java
@@ -4,6 +4,10 @@ public class Bowling {
 	private int puntaje;
 	private int lanzamientosJug1[] = new int[21];
 	private int lanzamientosJug2[] = new int[21];
+	private int pinos;
+	
+	private boolean strike=false;
+	
 	
 	private boolean tocaJugador1=true;//cuando es false le toca al jug2
 	
@@ -19,18 +23,49 @@ public class Bowling {
 
     	}
     	
-    	
+
     	if(pinosTirados==0) {
     		lanzamientosJug[lanzamientoIndice]=pinosTirados;
     		lanzamientoIndice++;
-    		
+
     		puntaje+=0;
+    	}else {
+    		
+    		if(lanzamientoIndice%2==0) {
+    			//primer lanzamiento
+    			if(pinosTirados==10) {
+        			//es strike
+            		lanzamientosJug[lanzamientoIndice]=pinosTirados;
+
+            		lanzamientoIndice++;
+            		lanzamientoIndice++;
+            		strike=true;
+            		puntaje+=10;
+        		}
+    			else {
+    				lanzamientosJug[lanzamientoIndice]=pinosTirados;
+    				puntaje+=pinosTirados;
+    				lanzamientoIndice++;
+    			}
+    		}
+    		
+    		
+    		
+    		lanzamientosJug[lanzamientoIndice]=pinosTirados;
+    		puntaje+=pinosTirados;
+    		lanzamientoIndice++;
+
+    		
     	}
     }
     
     
     public int getPuntaje() {
     	return puntaje;
+    }
+    
+    public int getCantidadPinosTirados() {
+    	return pinos;
     }
 
 	
