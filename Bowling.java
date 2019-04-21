89c8b8ce8338c737d08386e4ba68493dbb1fc40d
Pmastra <pabloo.agustin@gmail.com>
2019-04-21 17:15:08 -0300

    Aniadimos la opcion de 2 jugadores, falta terminar de armar la logica de esto

--- Bowling.java
+++ Bowling.java
@@ -1,20 +1,31 @@
 package Bowling;
 
-
 public class Bowling {
 	private int puntaje;
-	private int lanzamientos[] = new int[21];
+	private int lanzamientosJug1[] = new int[21];
+	private int lanzamientosJug2[] = new int[21];
+	
+	private boolean tocaJugador1=true;//cuando es false le toca al jug2
+	
 	private int lanzamientoIndice = 0;
-    
+     
     public void lanzarBola(int pinosTirados) {
+    	int[] lanzamientosJug;
+    	
+    	if(tocaJugador1) {
+    		lanzamientosJug=lanzamientosJug1;
+    	}else {
+    		lanzamientosJug=lanzamientosJug2;
+
+    	}
+    	
     	
     	if(pinosTirados==0) {
-    		lanzamientos[lanzamientoIndice]=pinosTirados;
+    		lanzamientosJug[lanzamientoIndice]=pinosTirados;
     		lanzamientoIndice++;
     		
     		puntaje+=0;
     	}
-    	
     }
     
     
