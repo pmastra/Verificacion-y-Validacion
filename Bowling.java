e68171f9fc4a6ca4f7f5f87362fcf188e04bc4b1
Pmastra <pabloo.agustin@gmail.com>
2019-04-21 17:02:24 -0300

    Solucion a test: TodasMalas

--- Bowling.java
+++ Bowling.java
@@ -1,11 +1,19 @@
 package Bowling;
 
+
 public class Bowling {
 	private int puntaje;
 	private int lanzamientos[] = new int[21];
 	private int lanzamientoIndice = 0;
     
     public void lanzarBola(int pinosTirados) {
+    	
+    	if(pinosTirados==0) {
+    		lanzamientos[lanzamientoIndice]=pinosTirados;
+    		lanzamientoIndice++;
+    		
+    		puntaje+=0;
+    	}
     	
     }
     
