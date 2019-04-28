89c89e7ed364b2c8700f99052935b10d055fce0d
Pmastra <pabloo.agustin@gmail.com>
2019-04-28 17:02:01 -0300

    Cambios pisados por version antigua

--- Bowling.java
+++ Bowling.java
@@ -12,39 +12,38 @@ public class Bowling {
         this.lanzamientoIndice++;
     }
      
-    public void calcularPuntaje() {
-    		
+    public int calcularPuntaje() {
+    		for(int i=0; i<lanzamientoIndice; i++){
+    			int pinosTirados=lanzamientosJug[i];
     			if(pinosTirados!=10) {
     			//Si entra aca es porque no hizo strike ni spare
     				if(pinosTirados==0) {
     					//Si entro aca es porque tiro 0 pinos
-    		    		lanzamientosJug[lanzamientoIndice]=pinosTirados;
-    		    		lanzamientoIndice++;
     		    		puntaje+=0;
     		    	}
     				else {
     					//Si entro aca es porque tiro algunos pinos
-    					lanzamientosJug[lanzamientoIndice]=pinosTirados;
-    					lanzamientoIndice++;
         				puntaje+=pinosTirados;
     				}
         		}
     			else {
-    				if(lanzamientoIndice%2!=0) {
-    					spare=true;
+    				if(lanzamientoIndice%2==0) {
+    					strike=true;
     					//Si entramos aca es porque hizo un spare
     					lanzamientosJug[lanzamientoIndice]=pinosTirados;
     					lanzamientoIndice++;
         				puntaje+=10;	
     				}
     				else {
-    					strike=true;
+    					spare=true;
     					//Si entramos aca es porque hizo un strike
     					lanzamientosJug[lanzamientoIndice]=pinosTirados;
     					lanzamientoIndice++;
         				puntaje+=10;
     				}
-    			} 
+    			}
+    		}
+    		return puntaje;
     }
     
     
